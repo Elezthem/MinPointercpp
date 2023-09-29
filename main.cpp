15e0234e14a224e8452eb6c9b6e185bf41283a18
ElezthemDev <97851451+Elezthem@users.noreply.github.com>
2023-09-29 07:46:44 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+void minimal(int* arr1, int len) {
+	int min = *arr1;
+	for (int a = 0; a < len; a++) {
+		if (min > *(arr1 + a))
+			min = *(arr1 + a);
+	}
+	cout << "Min: " << min << endl;
+}
+
+int main() {
+	setlocale(LC_ALL, "RU");
+	
+	int arr1[] = { 1, 2, 3, -4, 5, 6 };
+	minimal(arr1, 6);
+
+	return 0;
+}
